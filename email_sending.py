4b991833169449ba613758f438bb1ee060980991
Marie-Anne Grand <grand_m@epita.fr>
2019-12-02 01:20:03 +0100

    Improve email format to html

--- email_sending.py
+++ email_sending.py
@@ -1,43 +1,191 @@
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import config
-import smtplib
+import smtplib, ssl
 
 
 def send_email(individual):
-    user = config.email_user
+    sender = config.email_user
     password = config.email_password
+    receiver = individual.email
 
-    sent_from = user
-    to = [individual.email]
-    subject = 'OMG Super Important Message'
-    body = 'Ps: Sorry for the lake of design, I\'ll try to do better next year'
+    body = MIMEMultipart("alternative")
+    body["Subject"] = "OMG !!! You\'ve just receive a super Secret message"
+    body["From"] = sender
+    body["To"] = receiver
 
-    email_text = """\  
-    From: %s  
-    To: %s  
-    Subject: %s
+    plain_text = """\
+            This is the plain text version.
+            ------------------------------------------------
+            Christmas is coming! So it's time to do some shopping!!!!
+            ==> This year, {sender}, you have to give a gift to {chosen} <==
+            ------------------------------------------------
 
-    ------------------------------------------------
-    Only 26 days until Christmas! It's time to do some shopping!!
-    ==> This year, %s, you have to give a gift to %s <==
-    ------------------------------------------------
-    https://www.youtube.com/watch?v=V3EYjVPRClU
-    
-    !!! Merry Christmas !!!
+            !!! Merry Christmas !!!
     
     
-    -- 
-    xoxo
-    Santa
+            -- 
+            XOXO
+            Santa
+            
+            """.format(sender=individual.name, chosen=individual.chosen)
 
-    %s
-    """ % (sent_from, ", ".join(to), subject, individual.name, individual.chosen, body)
+    html_text = """\
+    <html>
+        <head>
+        </head>
+    <body>
+        <div class="es-wrapper-color">
+            <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+                <tbody>
+                    <tr>
+                        <td class="esd-email-paddings" valign="top">
+                            <table class="es-content es-preheader esd-header-popover" cellspacing="0" cellpadding="0" align="center">
+                                <tbody>
+                                    <tr>
+                                        <td class="es-adaptive esd-stripe" esd-custom-block-id="3839" align="center">
+                                            <table class="es-content-body" style="background-color: transparent;" width="600" cellspacing="0" cellpadding="0" align="center">
+                                                <tbody>
+                                                    <tr>
+                                                        <td class="esd-structure es-p10" align="left">
+                                                            <table cellspacing="0" cellpadding="0" width="100%">
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td class="esd-container-frame" width="580" align="left">
+                                                                            <table width="100%" cellspacing="0" cellpadding="0">
+                                                                                <tbody>
+                                                                                    <tr>
+                                                                                        <td class="es-infoblock esd-block-text" align="left">
+                                                                                            <p style="line-height: 14.4px; text-align: center;">Merry Christmas and happy new year!</p>
+                                                                                        </td>
+                                                                                    </tr>
+                                                                                </tbody>
+                                                                            </table>
+                                                                        </td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <table class="es-content" cellspacing="0" cellpadding="0" align="center">
+                                <tbody>
+                                    <tr></tr>
+                                    <tr>
+                                        <td class="esd-stripe" esd-custom-block-id="3802" align="center">
+                                            <table class="es-content-body" style="background-color: rgb(255, 255, 255);" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+                                                <tbody>
+                                                    <tr>
+                                                        <td class="esd-structure es-p20t es-p20b es-p20r es-p20l esd-checked" align="left" style="background-image:url(https://eornuu.stripocdn.email/content/guids/CABINET_6fb83e22902d396e5d3c21413ca00855/images/10431575242741109.png);background-position: center top; background-repeat: no-repeat;" background="https://eornuu.stripocdn.email/content/guids/CABINET_6fb83e22902d396e5d3c21413ca00855/images/10431575242741109.png">
+                                                            <table cellpadding="0" cellspacing="0" width="100%">
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td width="560" class="esd-container-frame" align="center" valign="top">
+                                                                            <table cellpadding="0" cellspacing="0" width="100%">
+                                                                                <tbody>
+                                                                                    <tr>
+                                                                                        <td align="left" class="esd-block-text">
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p style="text-align: center;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<strong><span style="font-size:22px;"> {chosen} </span></strong></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                            <p><br></p>
+                                                                                        </td>
+                                                                                    </tr>
+                                                                                </tbody>
+                                                                            </table>
+                                                                        </td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <table class="es-footer esd-footer-popover" cellspacing="0" cellpadding="0" align="center">
+                                <tbody>
+                                    <tr>
+                                        <td class="esd-stripe" esd-custom-block-id="3851" align="center">
+                                            <table class="es-footer-body" width="600" cellspacing="0" cellpadding="0" align="center">
+                                                <tbody>
+                                                    <tr>
+                                                        <td class="esd-structure es-p10t es-p20r es-p20l" esd-general-paddings-checked="true" align="left">
+                                                            <table width="100%" cellspacing="0" cellpadding="0">
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td class="esd-container-frame" width="560" valign="top" align="center">
+                                                                            <table width="100%" cellspacing="0" cellpadding="0">
+                                                                                <tbody>
+                                                                                    <tr>
+                                                                                        <td class="esd-block-text" align="center">
+                                                                                            <p style="font-size: 12px; line-height: 150%;">This is an automatic email, please do not reply to this email.</p>
+                                                                                            <p style="font-size: 12px; line-height: 150%;">If you have any reclamation about the chosen person or want to make suggestions, please feel free to send an email to: manne.grand@gmail.com<br></p>
+                                                                                        </td>
+                                                                                    </tr>
+                                                                                </tbody>
+                                                                            </table>
+                                                                        </td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="banner-toolbar"></div>
+    </body>
+</html> """.format(chosen=individual.chosen)
+
+    part1 = MIMEText(plain_text, "plain")
+    part2 = MIMEText(html_text, "html")
+
+    body.attach(part1)
+    body.attach(part2)
 
     try:
-        server = smtplib.SMTP_SSL(config.smtp, config.port_smtp)
-        server.ehlo()
-        server.login(user, password)
-        server.sendmail(sent_from, to, email_text)
-        server.close()
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(config.smtp, config.port_smtp, context=context) as server:
+            server.login(sender, password)
+            server.sendmail(sender, receiver, body.as_string())
+            server.close()
 
         print('Email sent!')
     except:
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import email_sending
 import human
 
 if __name__ == "__main__":
-    test_list = human.from_json("liste.json")
+    test_list = human.from_json("liste_test.json")
     choice.solver(test_list)
-    file = open("data.txt", "w")
+    file = open("2019_test.txt", "w")
     for x in test_list:
         buf = x.name + " must do a present to " + x.chosen + " & can't have:" + x.restriction + "\n"
         file.write(buf)
         email_sending.send_email(x)
     file.close()
+
